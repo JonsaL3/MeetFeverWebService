4cd1887578494b44ef30f7fbb14e931b0aaa2517
AlberHP <ahorcajadaperez@gmail.com>
2022-05-07 11:26:02 +0200

    Controladores de empleado añadidos

---
+++ ICP_PRACTICAS_DEFINITIVO/Controllers/EmpleadoController.cs
@@ -0,0 +1,97 @@
+﻿using MeetFeverWebService.Negocio;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+//using System.Web.Http.Cors;
+
+namespace MeetFeverWebService.Controllers
+{
+	[RoutePrefix("api/meetfever/empleado")]
+	//[EnableCors(origins: "*", headers: "*", methods: "*")]
+	public class EmpleadoController : ApiController
+	{
+
+		[HttpPut]
+		[Route("ActualizarEmpleado")]
+		public HttpResponseMessage ActualizarEmpleado([FromBody] object request)
+		{
+			try
+			{
+				PR_Negocio negocio = new PR_Negocio();
+				var response = negocio.ActualizarEmpleado(request);
+				return Request.CreateResponse(HttpStatusCode.OK, new { data = response });
+			}
+			catch (Exception ex)
+			{
+				return Request.CreateResponse(HttpStatusCode.InternalServerError, new { isError = true, data = ex.Message });
+			}
+		}
+
+		[HttpPut]
+		[Route("BorradoLogicoEmpleado")]
+		public HttpResponseMessage BorradoLogicoEmpleado([FromBody] object request)
+		{
+			try
+			{
+				PR_Negocio negocio = new PR_Negocio();
+				var response = negocio.BorradoLogicoEmpleado(request);
+				return Request.CreateResponse(HttpStatusCode.OK, new { data = response });
+			}
+			catch (Exception ex)
+			{
+				return Request.CreateResponse(HttpStatusCode.InternalServerError, new { isError = true, data = ex.Message });
+			}
+		}
+
+		[HttpDelete]
+		[Route("BorradoRealEmpleado")]
+		public HttpResponseMessage BorradoRealEmpleado([FromBody] object request)
+		{
+			try
+			{
+				PR_Negocio negocio = new PR_Negocio();
+				var response = negocio.BorradoRealEmpleado(request);
+				return Request.CreateResponse(HttpStatusCode.OK, new { data = response });
+			}
+			catch (Exception ex)
+			{
+				return Request.CreateResponse(HttpStatusCode.InternalServerError, new { isError = true, data = ex.Message });
+			}
+		}
+
+		[HttpPost]
+		[Route("InsertarEmpleado")]
+		public HttpResponseMessage InsertarEmpleado([FromBody] object request)
+		{
+			try
+			{
+				PR_Negocio negocio = new PR_Negocio();
+				var response = negocio.InsertarEmpleado(request);
+				return Request.CreateResponse(HttpStatusCode.OK, new { data = response });
+			}
+			catch (Exception ex)
+			{
+				return Request.CreateResponse(HttpStatusCode.InternalServerError, new { isError = true, data = ex.Message });
+			}
+		}
+
+		[HttpGet]
+		[Route("ObtenerEmpleadosPorIdEmpresa")]
+		public HttpResponseMessage ObtenerEmpleadosPorIdEmpresa([FromBody] object request)
+		{
+			try
+			{
+				PR_Negocio negocio = new PR_Negocio();
+				var response = negocio.ObtenerEmpleadosPorIdEmpresa(request);
+				return Request.CreateResponse(HttpStatusCode.OK, new { data = response });
+			}
+			catch (Exception ex)
+			{
+				return Request.CreateResponse(HttpStatusCode.InternalServerError, new { isError = true, data = ex.Message });
+			}
+		}
+
+
+	}
+}
