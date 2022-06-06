5361056293359f930870d4d95027afcf480057ae
JonsaL3 <jonsalogaming@gmail.com>
2022-06-06 10:13:47 +0200

    BORRADO CONFIRMAR

--- ICP_PRACTICAS_DEFINITIVO/Data/Experiencia.cs
+++ ICP_PRACTICAS_DEFINITIVO/Data/Experiencia.cs
@@ -30,6 +30,7 @@ namespace ICP_PRACTICAS_DEFINITIVO.Data
         public string Foto { get; set; }
         public int Id_Empresa { get; set; }
         public bool Eliminado { get; set; }
+        public bool Borrado_Solicitado { get; set; }
     
         public virtual Empresa Empresa { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
--- ICP_PRACTICAS_DEFINITIVO/Data/Model1.Context.cs
+++ ICP_PRACTICAS_DEFINITIVO/Data/Model1.Context.cs
@@ -1352,5 +1352,18 @@ namespace ICP_PRACTICAS_DEFINITIVO.Data
     
             return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("PA_Obtener_Registros", jSON_OUT, iNVOKERParameter, rETCODE, mENSAJE);
         }
+    
+        public virtual int PA_Borrado_Aprobado_Experiencia_Por_ID(string jSON_IN, ObjectParameter jSON_OUT, Nullable<int> iNVOKER, ObjectParameter rETCODE, ObjectParameter mENSAJE)
+        {
+            var jSON_INParameter = jSON_IN != null ?
+                new ObjectParameter("JSON_IN", jSON_IN) :
+                new ObjectParameter("JSON_IN", typeof(string));
+    
+            var iNVOKERParameter = iNVOKER.HasValue ?
+                new ObjectParameter("INVOKER", iNVOKER) :
+                new ObjectParameter("INVOKER", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("PA_Borrado_Aprobado_Experiencia_Por_ID", jSON_INParameter, jSON_OUT, iNVOKERParameter, rETCODE, mENSAJE);
+        }
     }
 }
